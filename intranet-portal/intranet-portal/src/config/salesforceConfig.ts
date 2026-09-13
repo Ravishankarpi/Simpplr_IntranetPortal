bcfd7ce0b17ed5e9953ac54eeafc4692c8a25234
ravi-getrecur <raviraw299@gmail.com>
2026-09-14 00:25:18 +0530

    secrets removed

--- intranet-portal/intranet-portal/src/config/salesforceConfig.ts
+++ intranet-portal/intranet-portal/src/config/salesforceConfig.ts
@@ -1,7 +1,7 @@
 export const salesforceConfig = {
   oauthDomain: "https://ornlfcu.my.salesforce.com",
   apiUrl: "https://ornlfcu.my.salesforce.com",
-  clientId: "3MVG9mclR62wycM0GDu7g08q6iYAERIVgD9bzEQp4rsNTaIoqehKRv9TInyIup2GD9.g3vby1fd0iLP_1cObr",
-  clientSecret: "ED63BF292022ADF516CC81DC24AEBA0A3F063835689806C1A67F280EBCBF8C82",
+  clientId: "--",
+  clientSecret: "--",
   sharePointSiteUrl: "https://sr7lt.sharepoint.com/sites/newww"
 };
