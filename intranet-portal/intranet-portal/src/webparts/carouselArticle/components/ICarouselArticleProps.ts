125b8e1a93b965436e4f5cfcfec1da2ce3d8355c
ravi-getrecur <raviraw299@gmail.com>
2026-09-14 13:00:18 +0530

    added initial level webpart for intranet portal

---
+++ intranet-portal/intranet-portal/src/webparts/carouselArticle/components/ICarouselArticleProps.ts
@@ -0,0 +1,7 @@
+export interface ICarouselArticleProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ intranet-portal/intranet-portal/src/webparts/latestPopularArticle/components/ILatestPopularArticleProps.ts
@@ -0,0 +1,7 @@
+export interface ILatestPopularArticleProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
